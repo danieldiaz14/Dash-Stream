2ca94fe8c879c162feee8d11788f87844fababd9
danieldiaz14 <danieldiaz129@gmail.com>
2018-12-21 10:35:49 -0800

    Learned about different routes. Browser, Hash, Memory

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return (
+        <div>
+            PageOne
+            <Link to="/pagetwo">Navigate to page two</Link>
+        </div>
+    )
+};
+
+const PageTwo = () => {
+    return (
+    <div>
+        PageTwo
+        <button>Click me</button>
+        <Link to="/">Navigate to Page one</Link>
+    </div>
+    )
+};
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+            <Route path="/" exact component={PageOne}/>
+            <Route path="/pagetwo" component={PageTwo}/>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App;
