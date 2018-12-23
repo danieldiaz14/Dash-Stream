41462845a433b46984b0ee6be38547f4e4490515
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-12-23 15:25:58 -0800

    Set state to sign in and out. Created Helper method to create button to sign in

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
-    
+    state = {
+        isSignedIn: null
+    };
+    // google restricts gapi because of the amount of people using it, because of it was must load it through componentDidMount to load it manually and using window for scope.
     componentDidMount() {
-        const id = '11648960831-ng4iufb7l7ga8ommnllmv8pej51a8q57.apps.googleusercontent.com';
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId: id,
+                clientId: '11648960831-ng4iufb7l7ga8ommnllmv8pej51a8q57.apps.googleusercontent.com',
                 scope: 'email'
-            });
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn: this.auth.isSignedIn.get() });
+            })
         });
     };
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return <div>I don't know if we are signed in</div>
+        } else if (this.state.isSignedIn) {
+            return <div>I am signed in</div>
+        } else {
+            return <div>I am not signed in.</div>
+        }
+    }
+
     render() {
+        console.log(this.isSignedIn);
         return (
-            <div className="item">Google Auth</div>
+            <div className="item">{this.renderAuthButton()}</div>
         )
     };
 }
