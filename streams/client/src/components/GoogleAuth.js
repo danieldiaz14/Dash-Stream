82f5d03721ffc1efe24dc56d6576b4e9fd9b875a
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-12-23 15:35:22 -0800

    Created button to sign in and dynamically update status

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -13,22 +13,26 @@ class GoogleAuth extends React.Component {
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
             })
         });
     };
 
+    onAuthChange = () => {
+        this.setState({isSignedIn: this.auth.isSignedIn.get() });
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in</div>
+            return <button>I don't know if we are signed in</button>
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in</div>
+            return <button>I am signed in</button>
         } else {
-            return <div>I am not signed in.</div>
+            return <button>I am not signed in.</button>
         }
     }
 
     render() {
-        console.log(this.isSignedIn);
         return (
             <div className="item">{this.renderAuthButton()}</div>
         )
