d7fdb4386d086662f54bc69873d6de6d7c0303b2
danieldiaz14 <danieldiaz129@gmail.com>
2018-12-26 15:37:13 -0800

    Working on StreamList component. Converted to Class based component and setup connect. Also called action creator in componentDidMount

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-    return <div>StreamList</div>
+class StreamList extends React.Component {
+    
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    render() {
+        return (
+            <div>StreamList</div>
+        )
+    }
 }
 
-export default connect(null, {})(StreamList);
+const mapStateToPros = (state) => {
+
+}
+export default connect(null, {fetchStreams})(StreamList);
