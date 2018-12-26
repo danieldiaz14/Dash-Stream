8b169272c27f5ffad7f5d5814f6c61cfb6eadf25
danieldiaz14 <danieldiaz129@gmail.com>
2018-12-26 15:46:38 -0800

    Worked on StreamList Component to show a list of available streams in our DB

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -6,16 +6,34 @@ class StreamList extends React.Component {
     
     componentDidMount() {
         this.props.fetchStreams();
-    }
+    };
 
+    renderList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera"/>
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            )
+        });
+    };
     render() {
         return (
-            <div>StreamList</div>
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+            </div>
         )
     }
 }
 
 const mapStateToPros = (state) => {
-
+    return { streams: Object.values(state.streams)};
 }
-export default connect(null, {fetchStreams})(StreamList);
+export default connect(mapStateToPros, {fetchStreams})(StreamList);
