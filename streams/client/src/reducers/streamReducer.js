2996cde6f53a3d302ec73b08c574bb09e6c18bca
danieldiaz14 <danieldiaz129@gmail.com>
2018-12-26 15:28:47 -0800

    Finished creating the Reducers. Using lodash to concatenate object and map the keys. method: _.mapKeys()

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
     CREATE_STREAM,
     FETCH_STREAMS,
@@ -8,12 +9,16 @@ import {
 
 export default (state = {}, {type, payload}) => {
     switch (type) {
+        case FETCH_STREAMS:
+        return {...state, ..._.mapKeys(payload,'id')};
         case FETCH_STREAM:
             return {...state, [payload.id]:payload};
         case CREATE_STREAM:
             return {...state, [payload.id]:payload};
         case EDIT_STREAM:
             return {...state, [payload.id]:payload};
+        case DELETE_STREAM:
+            return _.omit(state,payload);
         default:
             return state;
     }
