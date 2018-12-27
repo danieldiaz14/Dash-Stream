309a6ea5c9c253d5224b0ff02743d39be8b54d6d
danieldiaz14 <danieldiaz129@gmail.com>
2018-12-27 14:51:22 -0800

    Rendering stream info in stream edit as well as fetching the correct stream by id

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions/index'
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    render() {
+        const {stream} = this.props;
+        if (!stream) {
+            return <div>Loading..</div>
+        }
+        return (
+            <div>{stream.title}</div>
+        )
+    };
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    };
+};
+
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
---
+++ streams/client/src/components/streams/StreamForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+
+class StreamForm extends React.Component {
+    render() {
+        return (
+            <div>StreamForm</div>
+        )
+    }
+}
+
+export default connect(null)(StreamForm);
