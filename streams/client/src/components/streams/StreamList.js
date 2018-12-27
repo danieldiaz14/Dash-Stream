9d37189e73133f3364aa64b7920252ec57485ec0
danieldiaz14 <danieldiaz129@gmail.com>
2018-12-27 12:40:30 -0800

    Creating url-base navigation for edit and delete

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -13,12 +13,12 @@ class StreamList extends React.Component {
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
                         Edit
-                    </button>
-                    <button className="ui button negative">
+                    </Link>
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
                         Delete
-                    </button>
+                    </Link>
                 </div>
             )
         }
@@ -70,5 +70,6 @@ const mapStateToPros = (state) => {
         currentUserId: state.auth.userId,
         isSignedIn: state.auth.isSignedIn
     };
-}
+};
+
 export default connect(mapStateToPros, {fetchStreams})(StreamList);
