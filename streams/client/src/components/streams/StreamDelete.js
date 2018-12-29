81f4c52870392ef8078d2a0e546bf75324f808dd
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-12-28 21:25:19 -0800

    Adding Modal to Delete with ReactPortal

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { fetchStream, deleteStream } from '../../actions/index';
+import Modal from '../Modal';
 
-class StreamDelete extends React.Component {
-    componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
-    }
-
-    onDistroy = () => {
-        this.props.deleteStream(this.props.match.params.id);
-    }
-    render() {
-        const {stream} = this.props;
-        if (!stream) {
-            return <div>Loading..</div>
-        }
-        return (
-            <div>
-                <h3>Delete Stream: {stream.title}</h3>
-                <button className="ui button negative" onClick={this.onDistroy}>Delete</button>
-            </div>
-        )
-    }
-}
-
-const mapStateToProps = (state,ownProps) => {
-    return {
-        stream: state.streams[ownProps.match.params.id]
-    };
+const StreamDelete = () => {
+    return (
+        <div>
+            StreamDelete
+            <Modal/>
+        </div>
+    );
 };
 
-export default connect(mapStateToProps,{fetchStream,deleteStream})(StreamDelete)
+export default StreamDelete
