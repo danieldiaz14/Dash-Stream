1d19f67fa80675244b7759678ef335673adf9670
danieldiaz14 <danieldiaz129@gmail.com>
2019-01-02 14:39:56 -0800

    Added Flv.js to stream our video content in flash format

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
-import {fetchStream} from '../../actions';
+import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.videoRef = React.createRef();
+    }
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     };
-
-    renderContent = () => {
-
-    };
     render() {
-        console.log(this.props)
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        const {title, description} = this.props.stream;
+
         return (
-            <div>StreamShow</div>
-        )
+            <div>
+                <video ref={this.videoRef} style={{width: '100%'}} controls/>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        );
     }
 }
 
 const mapStateToProps = (state,OwnProps) => {
     return {
         stream: state.streams[OwnProps.match.params.id]
-    }
-}
+    };
+};
+
 export default connect(mapStateToProps, { fetchStream })(StreamShow);
